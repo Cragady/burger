efa93022dba0e17e62ccae67ea099d51fd33d3dc
Cragady <pinkdotdreamer@gmail.com>
2018-07-24 23:45:12 -0600

    added the model, controller, and orm setup

--- config/orm.js
+++ config/orm.js
@@ -1,16 +1,65 @@
 var connection = require("../config/connection.js");
 
+function qMarkers(num){
+    var arr = [];
+    for(var i = 0; i < num; i++){
+        arr.push("?");
+    };
+    return arr.toString();
+};
+
+function sqlEyesObj(ob){
+    var value = ob[key];
+    if(Object.hasOwnProperty.call(ob, key)){
+        if(typeof value === "string" && value.indexOf(" ") >= 0){
+            value = "'" + value + "'";
+        };
+        arr.push(key + "=" + value);
+    };
+    return arr.toString();
+};
+
 var orm = {
-    selectAll: function(tabIn, cb){
+    all: function(tabIn, cb){
         var querSt = "SELECT * FROM " + tabIn + ";";
         connection.query(querSt, function(err, res){
             if(err) throw err;
             cb(res);
         });
+    },
+    create: function(table, cols, vals, cb){
+        var querSt = "INSERT INTO" + table;
+        querSt += " (";
+        querSt += cols.toString();
+        querSt += ") ";
+        querSt += "VALUES (";
+        querSt += qMarkers(vals.length);
+        querSt += ") ";
+        connection.query(querSt, vals, function(err, res){
+            if (err) throw err;
+            cb(res);
+        });
+    },
+    update: function(table, objColVals, condition, cb){
+        var querSt = "UPDATE " + table;
+        querSt += " SET ";
+        querSt += sqlEyesObj(objColVals);
+        querSt += " WHERE ";
+        querSt += condition;
+        connection.query(querSt, function(err, res){
+            if(err) throw err;
+            cb(res);
+        });
+    },
+    delete: function(table, condition, cb){
+        var querSt = "DELETE FROM " + table;
+        querSt += " WHERE ";
+        querSt += condition;
+        connection.query(querSt, function(err, res){
+            if(err) throw err;
+            cb(res);
+        });
     }
-}
-
-// function insertOne();
-// function updateOne();
+};
 
 module.exports = orm;
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -3,11 +3,45 @@ var express = require('express'),
     router = express.Router();
 
 router.get("/", function(req, res){
-    burger.sAll(function(data){
+    burger.all(function(data){
         var handObj = {
             burgers: data
         };
         res.render('index', handObj);
+    });
+});
+
+router.post("/api/burgers", function(req, res){
+    burger.create([
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result){
+        res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    burger.udpate({
+        devoured: req.body.devoured
+    }, condition, function(result){
+        if(result.changedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+    burger.delete(condition, function(result){
+        if(result.affectedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
     });
 });
 
