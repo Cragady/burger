3e3b68f1368b43cd045e68be571079dd583a19b9
Cragady <pinkdotdreamer@gmail.com>
2018-07-26 02:16:06 -0600

    messing with seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,12 +2,12 @@ USE burgers_db;
 
 INSERT INTO burgers(burger_name, devoured)
 	values("Other Burger", false),
-        ("The Reubenator", true),
+        ("The Reubenator", false),
         ("The Dripper", true),
-        ("Cheez Berg", false),
+        ("Cheez Berg", true),
         ("Spicy Angus", true),
         ("Patty Melt", false),
-        ("Cheesy Mistake", true),
+        ("Cheesy Mistake", false),
         ("Gut Wrencher", true),
         ("Bum Bender", false),
         ("Greasenator", true)
